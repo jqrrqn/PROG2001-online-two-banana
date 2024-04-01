928562cc34296854beb2d62bd1fca68437b4157a
jqrrqn <157372248+jqrrqn@users.noreply.github.com>
2024-04-01 18:53:31 +1000

    Aesthetics
    Added background
    Added ground material
    Added game-win text

--- Assets/JarrenFolder/JarrenScripts/PlayerController_Jarren.cs
+++ Assets/JarrenFolder/JarrenScripts/PlayerController_Jarren.cs
@@ -87,7 +87,7 @@ public class PlayerController_Jarren : MonoBehaviour
         countText.text = "Gems: " + count.ToString();
 
         // Check if the count has reached or exceeded the win condition.
-        if (count >= 12)
+        if (count >= 6)
         {
             // Display the win text.
             winTextObject.SetActive(true);
