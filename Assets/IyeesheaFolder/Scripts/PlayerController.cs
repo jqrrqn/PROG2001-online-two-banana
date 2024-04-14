d7072e0847d6fa214c359aada48ecebc608d123e
Iyeeshea <i.thompson.15@student.scu.edu.au>
2024-04-14 17:31:24 +1000

    Movement script initiates

---
+++ Assets/IyeesheaFolder/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private int count;
+    private float movementX;
+    private float movementY;
+
+
+    public float speed = 0;
+
+   
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
+    {
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+    }
+
+
+}
+
